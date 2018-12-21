38c886789e3a2fb157a04b512b4bcd4c411c9461
EnricoRuggieroB <1bmeccenricorugbucchianico@gmail.com>
2018-12-21 22:07:16 +0000

    Algoritmo che dato in input il lato di un cubo, ne calcola il volume.

---
+++ cubo.py
@@ -0,0 +1,10 @@
+#helloworld
+
+#cubo.py
+
+#Algoritmo che dato in input il lato di un cubo, ne calcola il volume.
+
+lato = input("Inserire il lato del cubo. ") #Input del lato del cubo
+lato = int(lato) #Cast al tipo "int" (intero)
+volume = lato * lato * lato #Calcolo del volume del cubo
+print("Il volume del cubo di lato",lato,"e':",volume) #Stampa del volume del cubo
