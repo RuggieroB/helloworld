1ab37c361451bbcc642b7d3039acc7ab4b97655a
EnricoRuggieroB <1bmeccenricorugbucchianico@gmail.com>
2018-12-21 22:04:27 +0000

    Algoritmo che richiesto in input il nome dell'utente, lo saluta.

---
+++ hello.py
@@ -0,0 +1,8 @@
+#helloworld
+
+#hello.py
+
+#Algoritmo che richiesto in input il nome dell'utente, lo saluta.
+
+name = input("What's Your name? ") #Input del nome utente
+print("Hello,",name) #Saluto
