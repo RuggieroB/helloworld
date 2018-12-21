e5fcf98e404900a46caa9dffd5ca2da5afb0ada6
EnricoRuggieroB <1bmeccenricorugbucchianico@gmail.com>
2018-12-21 22:06:42 +0000

    Algoritmo che ricevuto in input un numero, ne stamperà il valore raddoppiato.

---
+++ doppio.py
@@ -0,0 +1,10 @@
+#helloworld
+
+#doppio.py
+
+#Algoritmo che ricevuto in input un numero, ne stamperà il valore raddoppiato.
+
+numero = input("Inserisci un Valore: ") #Input di un numero
+numero = int(numero) #Cast al tipo "int" (intero)
+doppio = numero * 2 #Raddoppio del Numero
+print("Il Doppio del Numero",numero,"e':",doppio) #Stampa del numero raddoppiato
